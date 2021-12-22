9c0ed01e3069fd9bdd814d5531dc068fe3879b9b
Savan Barbhaya <savanthegreat@gmail.com>
2021-12-23 00:56:32 +0530

    adding HTML in JS

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
+// Core Modules
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+// Top Level Code
 const API_Data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
-// console.log(API_Data);
+const OverviewPage = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf8"
+);
+const ProductPage = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf8"
+);
+
+// Server
 const server = http.createServer((req, res) => {
   const PathName = req.url;
   console.log("We got the request");
   console.log(`URL is ${PathName}`);
 
   if (PathName == "/Overview" || PathName == "/overview" || PathName == "/") {
-    res.end("<h1>Welcome To Overview page</h1>");
+    res.end(OverviewPage);
+    res.writeHead(201, {
+      "Content-Type": "text/html; charset=utf",
+    });
   } else if (PathName == "/Product" || PathName == "/product") {
-    res.end("<h1>Welcome To Product page</h1>");
-  } else if (PathName == "/API") {
-    // console.log("Caame Here");
+    res.end(ProductPage);
+    res.writeHead(201, {
+      "Content-Type": "text/html; charset=utf",
+    });
+  } else if (PathName == "/API" || PathName == "/api") {
     res.writeHead(200, {
       "Content-Type": "application/json",
-      "Mera-Man": "Kyu Tujhe chahe",
     });
     res.end("<h1>You Got all Data Below</h1>" + API_Data);
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html; charset=utf",
-      "Mera-Man": "Kyu Tujhe chahe",
     });
     res.end("GM");
   }
