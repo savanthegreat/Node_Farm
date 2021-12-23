0880687c5e0eace6cfe8bffa262f1fe4d85fa8c7
Savan Barbhaya <savanthegreat@gmail.com>
2021-12-23 22:43:16 +0530

    Building Web Application

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const http = require("http");
 const url = require("url");
 
 // Top Level Code
+const ReplaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
+
 const API_Data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
+const API_Data_Object = JSON.parse(API_Data);
 const OverviewPage = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
   "utf8"
@@ -13,6 +30,16 @@ const ProductPage = fs.readFileSync(
   `${__dirname}/templates/product.html`,
   "utf8"
 );
+const CardTempelate = fs.readFileSync(
+  `${__dirname}/templates/card.html`,
+  "utf8"
+);
+
+const CardsHtml = API_Data_Object.map((el) =>
+  ReplaceTemplate(CardTempelate, el)
+).join("");
+
+const OverviewOutputPage = OverviewPage.replace(/{%PRODUCT_CARDS%}/, CardsHtml);
 
 // Server
 const server = http.createServer((req, res) => {
@@ -20,26 +47,25 @@ const server = http.createServer((req, res) => {
   console.log("We got the request");
   console.log(`URL is ${PathName}`);
 
+  // Overview Page
   if (PathName == "/Overview" || PathName == "/overview" || PathName == "/") {
-    res.end(OverviewPage);
+    res.end(OverviewOutputPage);
     res.writeHead(201, {
       "Content-Type": "text/html; charset=utf",
     });
-  } else if (PathName == "/Product" || PathName == "/product") {
+  }
+
+  // Product Page
+  else if (PathName == "/Product" || PathName == "/product") {
     res.end(ProductPage);
     res.writeHead(201, {
       "Content-Type": "text/html; charset=utf",
     });
-  } else if (PathName == "/API" || PathName == "/api") {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-    res.end("<h1>You Got all Data Below</h1>" + API_Data);
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html; charset=utf",
     });
-    res.end("GM");
+    res.end("We are Not able to find any page as per your Request.");
   }
 });
 
